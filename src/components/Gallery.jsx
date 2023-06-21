96675119ed7088124a46f489f562f9a2ea118dbf
DraGonis <eddy.cui@outlook.com>
2023-06-21 21:48:05 +0300

    gallery works?

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,29 +1,40 @@
 import React, { useState, useEffect } from 'react';
 
 // Generate an array of image import promises dynamically
-const importImages = () => {
-  const importPromises = [];
-  for (let i = 1; i <= 58; i++) {
-    importPromises.push(import(`@/gallery/${i}.png`));
-  }
-  return Promise.all(importPromises);
-};
-
 function Gallery() {
     const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
   
     useEffect(() => {
       const importImages = async () => {
-        const importedImages = [];
-        for (let i = 1; i <= 58; i++) {
-          const image = await import(`@/gallery/${i}.png`);
-          importedImages.push(image.default);
+        const importPromises = [];
+        for (let i = 1; i <= 21; i++) {
+          importPromises.push(import(`@/gallery/${i}.png`));
         }
-        setData(importedImages.map((imgSrc, index) => ({ id: index + 1, imgSrc })));
+        try {
+          const importedImages = await Promise.all(importPromises);
+          const imageUrls = importedImages.map((image) => image.default);
+          const shuffledData = imageUrls.map((imgSrc, index) => ({
+            id: index + 1,
+            imgSrc,
+          })).sort(() => Math.random() - 0.5);
+          setData(shuffledData);
+          setIsLoading(false);
+        } catch (error) {
+          console.error('Error loading images:', error);
+        }
       };
   
       importImages();
     }, []);
+  
+    if (isLoading) {
+      return (
+        <div className="spinner-container">
+          <div className="spinner" />
+        </div>
+      );
+    }
   
     return (
       <div className="gallery">
