d59282978a12a1fd9d4e8a093f2e64d1c5f34f4c
dragonisdev <eddy.cui@outlook.com>
2025-05-13 17:58:11 +0300

    refactored hero section to not have a million divs

---
+++ src/songs.js
@@ -0,0 +1,80 @@
+
+export const songs = [
+  {
+    title: "nightrider",
+    cover: "/releases/nightrider.png",
+    streamUrl: "https://soundcloud.com/dragonis_music/nightrider-mastered-0dbtp-1",
+    mp3: "/songs/nightrider.mp3",
+    artwork: "/artworks/nightrider.png"
+  },
+  {
+    title: "Seraph",
+    cover: "/releases/seraph.png",
+    streamUrl: "https://fanlink.tv/seraph",
+    mp3: "/songs/seraph.mp3",
+    artwork: "/artworks/seraph.png"
+  },
+  {
+    title: "Stay Here",
+    cover: "/releases/stay_here.png",
+    streamUrl: "https://fanlink.tv/stay-here",
+    mp3: "/songs/stay_here.mp3",
+    artwork: "/artworks/stay_here.png"
+  },
+  {
+    title: "Aria",
+    cover: "/releases/aria.jpg",
+    streamUrl: "https://soundcloud.com/dragonis_music/aria",
+    mp3: "/songs/aria.mp3",
+    artwork: "/artworks/aria.jpg"
+  },
+  {
+    title: "Anchor",
+    cover: "/releases/anchor.jpg",
+    streamUrl: "https://fanlink.tv/anchored",
+    mp3: "/songs/anchor.mp3",
+    artwork: "/artworks/anchor.jpg"
+  },
+  {
+    title: "Epoch",
+    cover: "/releases/epoch.jpg",
+    streamUrl: "https://fanlink.tv/epoch21",
+    mp3: "/songs/epoch.mp3",
+    artwork: "/artworks/epoch.jpg"
+  },
+  {
+    title: "Caturday",
+    cover: "/releases/caturday.png",
+    streamUrl: "https://fanlink.tv/caturday",
+    mp3: "/songs/caturday.mp3",
+    artwork: "/artworks/caturday.png"
+  },
+  {
+    title: "Cryostasis",
+    cover: "/releases/cryostasis.png",
+    streamUrl: "https://fanlink.tv/cryostasis",
+    mp3: "/songs/cryostasis.mp3",
+    artwork: "/artworks/cryostasis.png"
+  },
+  {
+    title: "Twinkle",
+    cover: "/releases/twinkle.png",
+    streamUrl: "https://fanlink.tv/twinkle_1",
+    mp3: "/songs/twinkle.mp3",
+    artwork: "/artworks/twinkle.png"
+  },
+  {
+    title: "Hyperdrive",
+    cover: "/releases/hyperdrive.png",
+    streamUrl: "https://soundcloud.com/dragonis_music/hyperdrive-mastered-final-2",
+    mp3: "/songs/hyperdrive.mp3",
+    artwork: "/artworks/hyperdrive.png"
+  },
+  {
+    title: "Journey to Windrise",
+    cover: "/releases/Journey.png",
+    streamUrl: "https://fanlink.tv/journey-to-windrise",
+    mp3: "/songs/Journey_to_windrise.mp3",
+    artwork: "/artworks/Journey.png"
+  },
+];
